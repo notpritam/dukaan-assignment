4b3aa871840f22eafa6b5608514fe3052ac9a768
Pritam Sharma <notpritamsharma@gmail.com>
2024-07-06 16:12:19 +0530

    Starting

---
+++ index.js
@@ -0,0 +1,6 @@
+import OpenAI from "openai";
+
+// gets API Key from environment variable OPENAI_API_KEY
+const openai = new OpenAI({
+  apiKey: "sk-proj-B8qsABsRydCMcuuppMgdT3BlbkFJg6ifxFHeFavQMeKNpGda",
+});
